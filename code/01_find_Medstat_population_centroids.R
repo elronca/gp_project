6af32096f92680307bdf58938d14192fb1352ac4
Elias Ronca <eronca@gmail.com>
2019-10-18 11:57:40 +0200

    Repaired script that finds Medstat centers. It runs now and syntax is made much easier and better readable.

--- code/01_find_Medstat_population_centroids.R
+++ code/01_find_Medstat_population_centroids.R
@@ -11,20 +11,25 @@ library(ggspatial)
 # Read Medstat polygons
 
 MS_spdf <- st_read(file.path("data", "MedStat_GIS", "MedStat_Base_2011_region.shp"), 
-                   as_tibble = TRUE, options = "ENCODING=WINDOWS-1252") %>% 
+                   as_tibble = TRUE, options = "ENCODING=WINDOWS-1252",
+                   stringsAsFactors = FALSE) %>% 
   filter(KT != "FL")
+
+
 
 
 # Read number of persons by hectare
 
 pop_hectare <- read_csv2(file.path("data", "STATPOP2012B.csv"))
 
+
 # Hectares with 1 to 3 inhabitants are coded with 3 inhabitans for data securty reasons.
-# The get a more appropirate picture of the number of inhabitants we replace every 3 with a number of 1 to 3.
+# To get a more appropirate picture of real the number of inhabitants we replace every 3 with a random number of 1 to 3.
 
 (n_threes <- sum(pop_hectare$B12BTOT == 3))
 
 set.seed(84)
+
 replacement_values <- sample(1:3, n_threes, replace = TRUE)
 
 table(replacement_values)
@@ -34,17 +39,22 @@ pop_hectare[(pop_hectare$B12BTOT == 3), ]$B12BTOT <- replacement_values
 rm(n_threes, replacement_values)
 
 
+
+# Do the spatial stuff ----------------------------------------------------
+
+
 # Retrieve coordinate reference system from Medstat spatial polygon
 
 crs_MS_spdf <- st_crs(MS_spdf)[["proj4string"]]
 
-# Transform coordinates into spatial points
+
+# Transform coordinates of persons per hectare file into spatial points using coordinates of Medstat spatial polygon file
 
 pop_hectare_sp <- st_as_sf(x = pop_hectare, coords = c("X_KOORD", "Y_KOORD"), crs = crs_MS_spdf) %>% 
   select(RELI, B12BTOT, geometry)
 
 
-# Sample from the coordinates
+# Get a sample of the very large persons per hectare spatial points file
 
 pop_hectare_sp_sample <- pop_hectare_sp[sample(nrow(pop_hectare_sp), 10000), ]
 
@@ -52,153 +62,174 @@ pop_hectare_sp_sample <- pop_hectare_sp[sample(nrow(pop_hectare_sp), 10000), ]
 # Plot points into Medstat polygons (they fit nicely into the polygons)
 # Hence we use the same coordinates system for the medstat regions and the plot
 
-plot(st_geometry(MS_spdf), col = "red")
-plot(st_geometry(pop_hectare_sp_sample), type = "p", size = 0.1, add = TRUE)
+plot(st_geometry(MS_spdf), col = "yellow")
+plot(st_geometry(pop_hectare_sp_sample), type = "p", size = 0.1, col = "blue", add = TRUE)
+
+# If the blue points are located on the yellow map of Switzerland the coordinates system tranformation worked well
+
 
 rm(pop_hectare_sp_sample)
 
 
+
 # which points (hectares and related numbers of people) fall inside which polygon (Medstat regions)
 
-my_intersects <- st_intersects(MS_spdf, pop_hectare_sp)
+intersects <- st_intersects(MS_spdf, pop_hectare_sp)
 
-get_pop_centroids <- function(i, intersects) {
+
+# Using different methods to find the center of the Medstat shapefiles --------
+
+get_pop_centroids <- function(i, my_intersects, my_crs, pop_dens_file, map_file) {
+  
+  
+  if(FALSE) { # This is for debugging not part of the actual loop
+    
+    i = 1
+    my_intersects <- intersects
+    my_crs <- crs_MS_spdf
+    pop_dens_file <- pop_hectare_sp
+    map_file <- MS_spdf
+    
+  } # Debugging ends here
   
   pb$tick()$print()
   
-  index_region <- i
+  # i = the nth Medstat region
   
-  name_MedStat <- MS_spdf$MEDSTAT04[index_region]
+  name_MS_region_i <- map_file %>% pull(MEDSTAT04) %>% pluck(i)
   
-  my_intersects <- my_intersects[[index_region]]
+  intersects_region_i <- my_intersects %>% pluck(i)
   
-  my_region <- pop_hectare_sp[my_intersects, ]
+  coords_MS_region_i <- pop_dens_file %>% 
+    slice(intersects_region_i) %>% 
+    st_coordinates() %>% 
+    as_tibble()
   
+  B12BTOT <- pop_dens_file %>% 
+    slice(intersects_region_i) %>% 
+    pull(B12BTOT)
   
-  # Create unweighted population density centroid (yellow circle)
-  
-  my_region <- st_coordinates(pop_hectare_sp[my_intersects, ])
-  x <- mean(my_region[, 1])
-  y <- mean(my_region[, 2])
-  
-  center_crude <- tibble(x, y)
-  center_crude_sp <- st_as_sf(x = center_crude, coords = c("x", "y"), crs = crs_MS_spdf)
-  
-  
-  # Create weighted population density centroid (blue circle)
-  
-  my_region <- pop_hectare_sp[my_intersects, ]
-  my_region$weight <- my_region$B12BTOT / sum(my_region$B12BTOT)
-  
-  x <- weighted.mean(st_coordinates(my_region)[, 1], my_region$weight)
-  y <- weighted.mean(st_coordinates(my_region)[, 2], my_region$weight)
-  
-  center_weighted <- tibble(x, y)
-  center_weighted_sp <- st_as_sf(x = center_weighted, coords = c("x", "y"), crs = crs_MS_spdf)
+  coords_pop <- coords_MS_region_i %>% 
+    add_column(B12BTOT = B12BTOT)
   
   
-  # Calculate K Means clusters (red star)
-  
-  coords <- st_coordinates(pop_hectare_sp[my_intersects, ])
-  data <- st_set_geometry(pop_hectare_sp[my_intersects, ], NULL)
-  
-  my_tibble <- as_tibble(data.frame(data, coords)) %>% 
-    rename(X_Koord = X, Y_Koord = Y)
-  
-  weighted_tib_for_k_means = tibble(
-    X_Koord = rep(my_tibble$X_Koord, my_tibble$B12BTOT),
-    Y_Koord = rep(my_tibble$Y_Koord, my_tibble$B12BTOT)
-  )
-  
-  clust_info <- kmeans(weighted_tib_for_k_means, 2)
-  
-  k_means_center <- data.frame(X_Koord = clust_info$centers[1,1], Y_Koord = clust_info$centers[1,2])
   
   
-  # Find coordinates of region with highest number of inhabitants (green square)
+  # Create unweighted population density centroid (yellow circle) -----------
   
-  most_pop_place <- my_tibble %>% 
-    filter(B12BTOT == max(my_tibble$B12BTOT)) %>% 
-    filter(row_number()==1) %>% 
-    select(X_Koord, Y_Koord)
+  center_crude <- coords_MS_region_i %>% 
+    summarize_all(mean) %>%
+    st_as_sf(coords = c("X", "Y"), crs = my_crs)
   
-  my_plot <- ggplot() +
-    geom_sf(data = MS_spdf[index_region, ])+
-    geom_point(data = my_tibble, aes(x = X_Koord, y = Y_Koord, size = B12BTOT)) +
-    geom_point(data = k_means_center, aes(x = X_Koord, y = Y_Koord), colour = "red", shape = 8, stroke = 2, size = 6) +
-    geom_point(data = most_pop_place, aes(x = X_Koord, y = Y_Koord), colour = "green", shape = 0, stroke = 2, size = 6) +
-    geom_sf(data = center_crude_sp, colour = "yellow", size = 5, stroke = 2, shape = 21) +
-    geom_sf(data = center_weighted_sp, colour = "blue", size = 5, stroke = 2, shape = 21) +
+  
+  
+  # Create weighted population density centroid (blue circle) ---------------
+  
+  pop_weights <- pop_dens_file %>% 
+    slice(intersects_region_i) %>% 
+    mutate(weight = B12BTOT / sum(B12BTOT)) %>% 
+    pull(weight)
+  
+  center_pop_weighted <- coords_MS_region_i %>% 
+    map_dfr(weighted.mean, w = pop_weights) %>% 
+    st_as_sf(coords = c("X", "Y"), crs = my_crs)
+  
+  
+  
+  # Calculate K Means clusters (red star) -----------------------------------
+  
+  center_k_means <- coords_pop %>% 
+    uncount(B12BTOT) %>% 
+    kmeans(centers = 1) %>% 
+    .$centers %>% 
+    as_tibble() %>% 
+    st_as_sf(coords = c("X", "Y"), crs = my_crs)
+  
+  # with center = 1, this is the same as the weighted population density centroid.
+  
+  
+  
+  # Find coordinates of region with highest number of inhabitants (green square) --------
+  
+  center_most_pop_place <- coords_pop %>% 
+    filter(B12BTOT == max(B12BTOT)) %>% 
+    filter(row_number() == 1) %>% 
+    st_as_sf(coords = c("X", "Y"), crs = my_crs)
+  
+  
+  # Plot the centers on the Medstat region ------------------------------------
+  
+  MS_plot <- ggplot() +
+    geom_sf(data = map_file[i, ])+
+    geom_point(data = coords_pop, aes(x = X, y = Y, size = B12BTOT)) +
+    geom_sf(data = center_k_means, colour = "red", shape = 8, stroke = 2, size = 6) +
+    geom_sf(data = center_most_pop_place, colour = "green", shape = 0, stroke = 2, size = 6) +
+    geom_sf(data = center_crude, colour = "yellow", size = 5, stroke = 2, shape = 21) +
+    geom_sf(data = center_pop_weighted, colour = "blue", size = 5, stroke = 2, shape = 21) +
     annotation_scale(location = "br") +
-    ggtitle(name_MedStat)
+    ggtitle(name_MS_region_i)
   
-  
-  return(list(name_MedStat, center_crude_sp, center_weighted_sp, k_means_center, most_pop_place, my_plot))
+  return(list(name_MS_region = name_MS_region_i, 
+              center_crude = center_crude, 
+              center_pop_weighted = center_pop_weighted, 
+              center_k_means = center_k_means, 
+              center_most_pop_place = center_most_pop_place, 
+              MS_plot = MS_plot))
   
 }
 
 # Run function over all MedStat regions
 
 
-pb <- progress_estimated(705)
-# pb <- progress_estimated(1)
-my_results <- map(1:705, get_pop_centroids, intersects = my_intersects)
-# my_results <- map(1, get_pop_centroids, intersects = my_intersects)
+pb <- progress_estimated(nrow(MS_spdf))
 
-# Get Medstat names
-
-name_MedStat <- map(my_results, 1)
+my_results <- map(.x = seq_len(nrow(MS_spdf)), .f = get_pop_centroids, my_intersects = intersects, my_crs = crs_MS_spdf, pop_dens_file = pop_hectare_sp, map_file = MS_spdf)
 
 
-# Get population centroids
+# Plot different Medstat centers ------------------------------------------
 
-pop_centroids_avg_crd <- map(my_results, 2)
+plots_list <- map(my_results, "MS_plot")
 
-pop_centroids_avg_wgt <- map(my_results, 3)
-
-pop_centroids_km <- map(my_results, 4)
-
-pop_centroids_most_pop_place <- map(my_results, 5)
-
-
-# Rename list of population centroids
-
-names(pop_centroids_avg_crd) <- name_MedStat
-names(pop_centroids_avg_wgt) <- name_MedStat
-names(pop_centroids_km) <- name_MedStat
-names(pop_centroids_most_pop_place) <- name_MedStat
-
-# Get all plots of population centroids in Medstat spatial polygons
-
-plots_list <- map(my_results, 6)
-
-
-# Save plots
-
-GG_save_pdf = function(list, filename) {
-  #start pdf
-  pdf(filename)
+if(FALSE) {
   
-  #loop
-  for (p in list) {
-    print(p)
+  
+  # Save plots
+  
+  GG_save_pdf = function(list, filename) {
+    
+    #start pdf
+    
+    pdf(filename)
+    
+    #loop
+    
+    for (p in list) {
+      print(p)
+    }
+    
+    #end pdf
+    
+    dev.off()
+    
+    invisible(NULL)
+    
   }
   
-  #end pdf
-  dev.off()
+  GG_save_pdf(plots_list, file.path("output", "centroids.pdf"))
   
-  invisible(NULL)
 }
 
 
-# The blue circle is the weighted population centroid 
+# Save Medstat centers that will be used in further analysis ----------------------
 
-GG_save_pdf(plots_list, file.path("output", "centroids.pdf"))
+center_most_pop_place <- map(my_results, "center_most_pop_place")
 
-save(pop_centroids_km, file = file.path("workspace", "k_means_centroid.RData"))
-save(pop_centroids_most_pop_place, file = file.path("workspace", "pop_centroids_most_pop_place.RData"))
+save(center_most_pop_place, file = file.path("workspace", "center_most_pop_place.RData"))
 
-rm("crs_MS_spdf", "get_pop_centroids", "GG_save_pdf", "MS_spdf", 
-  "my_intersects", "my_results", "name_MedStat", "pb", "plots_list", 
-  "pop_centroids_avg_crd", "pop_centroids_avg_wgt", "pop_centroids_km", 
-  "pop_centroids_most_pop_place", "pop_hectare", "pop_hectare_sp")
+
+# Clear workspace ---------------------------------------------------------
+
+rm("center_most_pop_place", "crs_MS_spdf", "get_pop_centroids", 
+     "intersects", "MS_spdf", "my_results", "pb", "plots_list", "pop_hectare", 
+     "pop_hectare_sp")
+
